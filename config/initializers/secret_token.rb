b5dfc974b63b5e1811b26b8948f3f0eac20df9b2
TLN <tln_sys@yahoo.com>
2012-07-04 11:51:03 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '83f955512e3e0dbb0563617a3402a0885f4e9eb8b067b84e96c014e89d861d29531893bb8130c60826cdd3eac0f89eb34dfde063060117a44719a6e8b9adf41e'
